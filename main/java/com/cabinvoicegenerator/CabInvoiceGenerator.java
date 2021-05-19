3e624aecd0bf433a3e0e3c3682b5249dc461e7d6
Nishi Gautam <nishigautam.nishu2@gmail.com>
2021-05-19 17:12:21 +0530

    Ability to return Minimum fare if less distance covered

--- main/java/com/cabinvoicegenerator/CabInvoiceGenerator.java
+++ main/java/com/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -3,8 +3,10 @@ package com.cabinvoicegenerator;
 public class CabInvoiceGenerator {
     private static final double COST_PER_KM = 10.0;
     private static final int COST_PER_MIN = 1;
+    private static final double MIN_FARE = 5.0;
 
     public double calculateFare(double distance, int time) {
-        return distance * COST_PER_KM + time * COST_PER_MIN;
+        double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
+        return Math.max(totalFare, MIN_FARE);
     }
 }
--- test/java/com/cabinvoicegenerator/InvoiceGeneratorTest.java
+++ test/java/com/cabinvoicegenerator/InvoiceGeneratorTest.java
@@ -21,4 +21,13 @@ public class InvoiceGeneratorTest {
         double fare = cabInvoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(59, fare, 0.0);
     }
+
+    @Test
+    public void givenLessDistanceAndTime_ReturnMinimumFare() {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        double distance = 0.01;
+        int time = 1;
+        double fare = cabInvoiceGenerator.calculateFare(distance,time);
+        Assert.assertEquals(5, fare, 0.0);
+    }
 }
